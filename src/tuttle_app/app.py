c1b8e9767e62e12cc885032a0c2ba9b6359ebd9d
Christian Staudt <mail@clstaudt.me>
2022-01-11 11:22:00 +0000

    added test commands

--- src/tuttle_app/app.py
+++ src/tuttle_app/app.py
@@ -8,6 +8,10 @@ from toga.style.pack import COLUMN, ROW
 
 class Tuttle(toga.App):
 
+    def dummy_command(self, widget):
+        """A dummy command for demo purposes"""
+        print("Command triggered")
+
     def startup(self):
         """
         Construct and show the Toga application.
@@ -16,10 +20,36 @@ class Tuttle(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
+        # Main window
         main_box = toga.Box()
-
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
+
+        ## Commands
+        menu_file = toga.Group("File")  
+        menu_view = toga.Group("View")
+        
+        cmd_example_file = toga.Command(
+            self.dummy_command,
+            label='Test',
+            tooltip='Test command',
+            group=menu_file
+        )
+
+        cmd_example_view = toga.Command(
+            self.dummy_command,
+            label='Test',
+            tooltip='Test command',
+            group=menu_view
+        )
+
+        commands = (
+            cmd_example_view,
+            cmd_example_file
+        )
+        self.app.commands.add(*commands)
+        self.app.main_window.toolbar.add(*commands)
+
         self.main_window.show()
 
 
